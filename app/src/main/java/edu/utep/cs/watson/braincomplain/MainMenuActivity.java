ee078ad0456356a2f6f1ac6a56f4404f6d7e1c65
Jason Carlson <jcarlson08@gmail.com>
2015-10-19 19:58:52 -0500

    Initial commit

---
+++ app/src/main/java/edu/utep/cs/watson/braincomplain/MainMenuActivity.java
@@ -0,0 +1,114 @@
+package edu.utep.cs.watson.braincomplain;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import java.util.ArrayList;
+
+public class MainMenuActivity extends AppCompatActivity {
+    ArrayList<String> menu_list = new ArrayList<String>(3);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        menu_list.add(getString(R.string.ask_watson));
+        menu_list.add(getString(R.string.maps_psych));
+        menu_list.add(getString(R.string.call_SAMHSA));
+        ListView listView = (ListView)findViewById(R.id.listView);
+        ArrayAdapter <String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu_list);
+        listView.setAdapter(arrayAdapter);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        AdapterView.OnItemClickListener
+                mMessageClickedHandler =
+                new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView parent,
+                                            View v,
+                                            int position,
+                                            long id) {
+                        switch(position) {
+                            case 0: startAskWatson();
+                                    break;
+                            case 1: startMapIntent();
+                                    break;
+                            case 2: startCallIntent();
+                                    break;
+                        }
+                    }
+                };
+
+        listView.setOnItemClickListener(mMessageClickedHandler);
+    }
+
+    private void startAskWatson() {
+        Intent intent = new Intent(this, WatsonQueryActivity.class);
+        startActivity(intent);
+
+    }
+
+    private void startMapIntent() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:0,0?q=psychiatrist+office"));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+    }
+
+    private void startCallIntent() {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + getString(R.string.SAMHSA_number)));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+    }
+
+    private void startQuitIntent() {
+        finish();
+        System.exit(0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.universal_expandable_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_ask_watson) {
+            startAskWatson();
+        }
+        if (id == R.id.action_maps_psych) {
+            return true;
+        }
+        if (id == R.id.action_call_apa) {
+            return true;
+        }
+        if (id == R.id.action_quit) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/edu/utep/cs/watson/braincomplain/WatsonQueryActivity.java
@@ -0,0 +1,111 @@
+package edu.utep.cs.watson.braincomplain;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class WatsonQueryActivity extends AppCompatActivity implements WatsonQueryFragment.WatsonQueryCallbacks {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watson_query);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new WatsonQueryFragment())
+                    .commit();
+        }
+    }
+
+    private void startAskWatson() {
+        Intent intent = new Intent(this, WatsonQueryActivity.class);
+        startActivity(intent);
+
+    }
+
+    private void startMapIntent() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:0,0?q=psychiatrist+office"));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    private void startCallIntent() {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + getString(R.string.SAMHSA_number)));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+    }
+
+    private void startQuitIntent() {
+        finish();
+        System.exit(0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.universal_expandable_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_ask_watson) {
+            startAskWatson();
+        }
+        if (id == R.id.action_maps_psych) {
+            return true;
+        }
+        if (id == R.id.action_call_apa) {
+            return true;
+        }
+        if (id == R.id.action_quit) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+    }
+
+    @Override
+    public void onPreExecute() {
+
+    }
+
+    @Override
+    public void onProgressUpdate(int percent) {
+
+    }
+
+    @Override
+    public void onCancelled() {
+
+    }
+
+    @Override
+    public void onPostExecute() {
+
+    }
+}
